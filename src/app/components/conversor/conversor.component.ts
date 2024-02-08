82772df0743377903ae2625f79aa2497dadf42f3
C41m <caiofernandobs@gmail.com>
2024-02-07 21:51:40 -0300

    Api BCB Integrada

--- src/app/components/conversor/conversor.component.ts
+++ src/app/components/conversor/conversor.component.ts
@@ -1,12 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface ApiResponse {
+  '@odata.context': string;
+  value: { cotacaoCompra: number }[];
+}
 
 @Component({
   selector: 'app-conversor',
   templateUrl: './conversor.component.html',
-  styleUrl: './conversor.component.scss'
+  styleUrls: ['./conversor.component.scss'],
 })
 export class ConversorComponent {
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.conversor();
+  }
 
-    
+  formatDate(data: Date): string {
+    const day = String(data.getDate()).padStart(2, '0');
+    const month = String(data.getMonth() + 1).padStart(2, '0');
+    const year = data.getFullYear();
+    return `${month}-${day}-${year}`;
+  }
+
+  todayDate = this.formatDate(new Date());
+  money = 'EUR';
+
+  apiUrl = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='${this.money}'&@dataInicial='${this.todayDate}'&@dataFinalCotacao='${this.todayDate}'&$top=100&$skip=4&$format=json&$select=cotacaoCompra`;
+
+  responseData: ApiResponse = { '@odata.context': '', value: [] };
+
+  conversor(): void {
+    this.http.get<ApiResponse>(this.apiUrl).subscribe((data) => {
+      this.responseData = data;
+      const quote = data.value[0].cotacaoCompra;
+      console.log('Cotação:', quote);
+    });
+  }
 }
